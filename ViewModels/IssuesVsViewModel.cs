65ac1ecc485892875db25e815bebfa8c20a2b60b
KianKazama <104407700+kianlibunao@users.noreply.github.com>
2024-05-08 16:04:14 +0800

    Add files via upload

---
+++ ViewModels/IssuesVsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SGCTicketSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SGCTicketSystem.ViewModels
+{
+    public class IssuesVsViewModel
+    {
+        public Issues Issuess { get; set; }
+        public IEnumerable<Priority> Priorities { get; set; }
+        public IEnumerable<Branch> Branches { get; set; }
+        public IEnumerable<Department> Departments { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Status> Statuses { get; set; }
+
+
+        //public IEnumerable<Technical> Technicals { get; set; }
+
+    }
+}
---
+++ ViewModels/UserVsUserTypeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SGCTicketSystem.Models;
+
+
+namespace SGCTicketSystem.ViewModels
+{
+    public class UserVsUserTypeViewModel
+    {
+        public User User { get; set; }
+        public IEnumerable<UserType> UserTypes { get; set; }
+
+
+    
+    }
+
+}
